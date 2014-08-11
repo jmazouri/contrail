1fdc479e35f27944965856e8d1a05129beb76f4a
John Mazouri <jmazouri@users.noreply.github.com>
2014-08-11 16:45:48 -0400

    Initial Commit

---
+++ ConTrail/Item.cs
@@ -0,0 +1,11 @@
+namespace ConTrail
+{
+    public class Item
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public float Value { get; set; }
+
+        public Vitals VitalMod { get; set; }
+    }
+}
