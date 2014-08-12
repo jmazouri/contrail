1f7984c7a270bf02d10baccc5ba24f71a4415fc1
John Mazouri <jmazouri@users.noreply.github.com>
2014-08-12 16:55:07 -0400

    Added a few more properties to Items Changed how vitals work Added some tests Made item importer work

--- ConTrail/Item.cs
+++ ConTrail/Item.cs
@@ -1,3 +1,6 @@
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+
 namespace ConTrail
 {
     public class Item
@@ -6,6 +9,32 @@ namespace ConTrail
         public string Description { get; set; }
         public float Value { get; set; }
 
+        public int Uses { get; set; }
+
+        public int MaxCooldown { get; set; }
+        public int Cooldown { get; set; }
+
         public Vitals VitalMod { get; set; }
+
+        public UseTypes UseType
+        {
+            get { return (Uses >= 0 ? UseTypes.Consumable : UseTypes.Infinite); }
+        }
+
+        public bool CanUse
+        {
+            get { return (UseType == UseTypes.Infinite || (Uses > 0)); }
+        }
+    }
+
+    public enum UseTypes
+    {
+        Consumable,
+        Infinite
+    }
+
+    public enum ItemTypes
+    {
+        Standard
     }
 }
---
+++ ConTrailTests/ImporterTests.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using ConTrail.Importers;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ConTrailTests
+{
+    [TestClass]
+    public class ImporterTests
+    {
+        [TestMethod]
+        public void TestItemImport()
+        {
+            var Items = ItemImporter.Import();
+            Assert.IsNotNull(Items);
+            Assert.IsTrue(Items.Count > 0);
+
+            foreach (var Category in Items)
+            {
+                Assert.IsTrue(Category.Value.Count > 0);
+            }
+        }
+    }
+}
